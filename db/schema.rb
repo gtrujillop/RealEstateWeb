9e6fcaa3bbbf393066d6f01ea06fe57257d4a06a
Gaston Trujillo <gastus2@hotmail.com>
2015-02-14 11:10:38 -0500

    Add password digest field to User model and remove extra password.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212034230) do
+ActiveRecord::Schema.define(version: 20150212035926) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name",              limit: 255
@@ -54,17 +54,16 @@ ActiveRecord::Schema.define(version: 20150212034230) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",              limit: 255
-    t.string   "password",              limit: 255
-    t.string   "dni_number",            limit: 255
-    t.string   "first_name",            limit: 255
-    t.string   "last_name",             limit: 255
-    t.string   "email",                 limit: 255
-    t.string   "phone",                 limit: 255
+    t.string   "username",        limit: 255
+    t.string   "dni_number",      limit: 255
+    t.string   "first_name",      limit: 255
+    t.string   "last_name",       limit: 255
+    t.string   "email",           limit: 255
+    t.string   "phone",           limit: 255
     t.date     "birth_date"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.string   "password_confirmation", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
   end
 
   create_table "zones", force: :cascade do |t|
