e88f272051f294c9790bc87309adc332d4dcd0b2
Gaston Trujillo <gastus2@hotmail.com>
2015-02-14 11:09:29 -0500

    Green validation specs for User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+	
+	attr_accessor :password							 
+
+	validates_presence_of :username, :first_name, :last_name
+
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 5 }, on: :create
+
+	validates :email, uniqueness: true, presence: true
+	validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+
+	describe "password validations" do
+		it "fails because no password" do
+    	expect(User.new({ username: "user_1", first_name: "Juan", last_name: "Perez" }).save).to be false
+  	end
+
+  	it "fails because password is short" do
+    	expect(User.new({username: "user_1", first_name: "Juan", last_name: "Perez", password: 'blah'}).save).to be false
+  	end
+
+  	it "fails because password confirmation does not match" do
+    	expect(User.new({username: "user_1", first_name: "Juan", last_name: 
+    						"Perez", password: 'password_1', password_confirmation: 'password_2'}).save).to be false
+  	end
+
+  	it "saves because password is valid" do
+    	expect(User.new({username: "user_1", first_name: "Juan", last_name: 
+    						"Perez", password: 'password_1', password_confirmation: 'password_1'}).save).to be false
+  	end
+	end
+
+	describe "presence validations" do
+
+		it { should validate_presence_of(:username) }
+		it { should validate_presence_of(:first_name) }
+		it { should validate_presence_of(:last_name) }
+		it { should validate_presence_of(:email) }
+
+
+	end
+
+	describe "format validations" do
+
+		it do
+			should allow_value('my_email@email.com').
+				for(:email).
+				on(:create)	
+		end
+
+	end
+  
 end
