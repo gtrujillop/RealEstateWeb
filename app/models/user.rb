fc350eab7ee71cc14ee60b2eaea2e14c4c97d047
Gaston Trujillo <gastus2@hotmail.com>
2015-02-14 12:14:44 -0500

    Add validation to birth_date on User model and fix spec.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ActiveRecord::Base
 	
-	attr_accessor :password							 
-
 	validates_presence_of :username, :first_name, :last_name
-
+	validates :username, presence: true, length: { maximum: 8 }
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 5 }, on: :create
-
+	validates :birth_date, date: true
 	validates :email, uniqueness: true, presence: true
 	validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,21 +4,21 @@ describe User do
 
 	describe "password validations" do
 		it "fails because no password" do
-    	expect(User.new({ username: "user_1", first_name: "Juan", last_name: "Perez" }).save).to be false
+    	expect(User.new({ username: "user_1", first_name: "Juan", last_name: "Perez", email:'my_email@email.com' }).save).to be false
   	end
 
   	it "fails because password is short" do
-    	expect(User.new({username: "user_1", first_name: "Juan", last_name: "Perez", password: 'blah'}).save).to be false
+    	expect(User.new({username: "user_1", first_name: "Juan", last_name: "Perez", password: 'blah', email:'my_email@email.com'}).save).to be false
   	end
 
   	it "fails because password confirmation does not match" do
     	expect(User.new({username: "user_1", first_name: "Juan", last_name: 
-    						"Perez", password: 'password_1', password_confirmation: 'password_2'}).save).to be false
+    						"Perez", password: 'password_1', password_confirmation: 'password_2', email:'my_email@email.com'}).save).to be false
   	end
 
   	it "saves because password is valid" do
     	expect(User.new({username: "user_1", first_name: "Juan", last_name: 
-    						"Perez", password: 'password_1', password_confirmation: 'password_1'}).save).to be false
+    						"Perez", password: 'password_1', password_confirmation: 'password_1', email:'my_email@email.com'}).save).to be false
   	end
 	end
 
@@ -40,6 +40,19 @@ describe User do
 				on(:create)	
 		end
 
+		it { should validate_length_of(:username).is_at_most(8) }
+
+
+	end
+
+	describe "date validations" do
+		it 'does not allow save users with invalid dates' do
+    	expect(User.new({ username: "user_1", first_name: "Juan", 
+    										last_name: "Perez", birth_date: 'blah', 
+    										email:'my_email@email.com', 
+    										password: 'password_1', 
+    										password_confirmation: 'password_1'}).save).to be false
+		end
 	end
   
 end
