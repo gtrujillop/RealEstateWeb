5ff8a87eed897a775069c93ad0a12d242dc9056d
Gaston Trujillo <gastus2@hotmail.com>
2015-02-17 19:42:12 -0500

    Add validations to Neighbor model.

--- app/models/neighbor.rb
+++ app/models/neighbor.rb
@@ -1,2 +1,7 @@
 class Neighbor < ActiveRecord::Base
+
+	validates :name, presence: true, length: { maximum: 140 }
+	validates :stratus, presence: true, numericality: { only_integer: true }
+
+	has_many :property
 end
--- spec/models/neighbor_spec.rb
+++ spec/models/neighbor_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Neighbor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Neighbor do
+
+  describe "presence validations" do
+
+		it { should validate_presence_of(:name) }
+		it { should validate_presence_of(:stratus) }
+
+	end
+
+	describe "number validations" do
+		it { should validate_numericality_of(:stratus) }
+		
+	end
+
+	describe 'length validations' do
+		it { should validate_length_of(:name).is_at_most(140) }
+	end
+
+
 end
