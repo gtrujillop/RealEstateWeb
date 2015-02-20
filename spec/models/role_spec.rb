4766f23995734dd33ab44fa9fb42684334c0bdfd
Gaston Trujillo <gastus2@hotmail.com>
2015-02-19 21:15:25 -0500

    Add Role and UserRole Model and spec with associations.

---
+++ spec/models/role_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Role do
+	describe "presence validations" do
+		
+		it { should validate_presence_of(:name) }
+		it { should validate_presence_of(:description) }
+			
+	end
+
+	describe "length validations" do
+		it { should validate_length_of(:name).is_at_most(30) }
+	end
+
+	describe "relation with user" do
+		it { should have_many(:users).through(:user_roles) }
+	end
+
+end
