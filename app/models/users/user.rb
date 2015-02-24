d1193ea4f34fccc19bb70170211d06c9789f2822
Gaston Trujillo <gastus2@hotmail.com>
2015-02-23 21:53:52 -0500

    Add after_save validation to update is_active as true.

--- app/models/users/user.rb
+++ app/models/users/user.rb
@@ -7,10 +7,13 @@ class User < ActiveRecord::Base
 	validates :birth_date, date: true
 	validates :email, uniqueness: true, presence: true
 	validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
-	validates :is_active, presence: true
+
+	after_save :set_is_active
 
 	has_many :user_roles
 	has_many :roles, through: :user_roles
 
-
+	def set_is_active
+		self.update_column(:is_active, true)	end
+	private :set_is_active
 end
