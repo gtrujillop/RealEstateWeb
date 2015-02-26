0888090a0d25412903287fca7d3afaf4592857cc
Gaston Trujillo <gastus2@hotmail.com>
2015-02-26 18:32:28 -0500

    Modify db schema using sorcery user model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219022850) do
+ActiveRecord::Schema.define(version: 20150226225332) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name",              limit: 255
@@ -68,19 +68,21 @@ ActiveRecord::Schema.define(version: 20150219022850) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",        limit: 255
-    t.string   "dni_number",      limit: 255
-    t.string   "first_name",      limit: 255
-    t.string   "last_name",       limit: 255
-    t.string   "email",           limit: 255
-    t.string   "phone",           limit: 255
+    t.string   "username",         limit: 255,                 null: false
+    t.string   "first_name",       limit: 255
+    t.string   "last_name",        limit: 255,                 null: false
+    t.string   "identity",         limit: 255
+    t.string   "email",            limit: 255,                 null: false
     t.date     "birth_date"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.string   "password_digest", limit: 255
-    t.boolean  "is_active",       limit: 1
-    t.string   "type",            limit: 255
+    t.boolean  "active",           limit: 1,   default: false
+    t.string   "type",             limit: 255
+    t.string   "crypted_password", limit: 255
+    t.string   "salt",             limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   create_table "zones", force: :cascade do |t|
     t.string   "name",              limit: 255
