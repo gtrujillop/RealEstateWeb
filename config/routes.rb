c00e8e5c1045f995bfeed17d9b3384b3d1346bbe
Gaston Trujillo <gastus2@hotmail.com>
2015-02-26 23:07:51 -0500

    Add routes for create login and logout actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
   resources :users, only: [:new, :create, :show]
+
+  resources :sessions, only: [:create]
+  get 'login' => 'sessions#new', :as => :login
+	post 'logout' => 'sessions#destroy', :as => :logout 
 end
