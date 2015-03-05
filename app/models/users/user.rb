cb3df3c24795a93d7403b8c56aab7f7c3a1f3759
Gaston Trujillo <gastus2@hotmail.com>
2015-03-04 20:33:28 -0500

    Move User to users folder to apply STI.

---
+++ app/models/users/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+  validates :password,                presence: true, confirmation: true, length: { minimum: 3}
+  validates :email,                   presence: true, uniqueness: true, format:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+  validates :password_confirmation,   presence: true
+  validates :birth_date, date: true
+
+  after_save :set_is_active
+
+	has_many :user_roles
+	has_many :roles, through: :user_roles
+
+	def set_is_active
+		self.update_column(:active, true)	
+	end
+	private :set_is_active
+end
---
+++ spec/models/users/user_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+describe User do
+
+	describe "password validations" do
+		it "fails because no password" do
+    	expect(User.new({ username: "user_1",birth_date: '11-09-1988', first_name: "Juan", last_name: "Perez", email:'my_email@email.com'}).save).to be false
+  	end
+
+  	it "fails because password is short" do
+    	expect(User.new({username: "user_1",birth_date: '11-09-1988', first_name: "Juan", last_name: "Perez", password: 'blah', email:'my_email@email.com'}).save).to be false
+  	end
+
+  	it "fails because password confirmation does not match" do
+    	expect(User.new({username: "user_1", first_name: "Juan",birth_date: '11-09-1988', last_name: 
+    						"Perez", password: 'password_1', password_confirmation: 'password_2', email:'my_email@email.com'}).save).to be false
+  	end
+
+  	it "saves because password is valid" do
+    	expect(User.new({username: "user_1",birth_date: '11-09-1988', first_name: "Juan", last_name: 
+    						"Perez", password: 'password_1', password_confirmation: 'password_1', email:'my_email@email.com'}).save).to be true
+  	end
+	end
+
+	describe "presence validations" do
+
+		it { should validate_presence_of(:email) }
+		it { should validate_presence_of(:password) }
+		it { should validate_presence_of(:password_confirmation) }
+
+	end
+
+	describe "format validations" do
+
+		it do
+			should allow_value('my_email@email.com').
+				for(:email).
+				on(:create)	
+		end
+
+		it { should validate_length_of(:password).is_at_least(3) }
+
+	end
+
+	describe "relation with role" do
+		it { should have_many(:roles).through(:user_roles) }
+	end
+
+	describe 'after validations' do
+		it 'sets is_active value before saving the user' do
+			user = User.new({ username: "user_1", first_name: "Juan", 
+    										last_name: "Perez", birth_date: '11-09-1988', 
+    										email:'my_email@email.com', 
+    										password: 'password_1', 
+    										password_confirmation: 'password_1'})
+			user.save
+			expect(User.last.active).to be_truthy
+		end
+	end
+  
+end
