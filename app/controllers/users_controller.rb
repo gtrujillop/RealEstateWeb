f7e4c7700529ce1cc19cad840f7f64af53bd46c7
Gaston Trujillo <gastus2@hotmail.com>
2015-03-07 02:39:40 -0500

    Add before_filter on save and update to set date format and permit type as user param.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_filter :require_login, :only => [:new, :create]
+  before_action :set_date_format, only: [:create, :update]
 
 	def new
 		@user = User.new
@@ -9,7 +10,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = 'Bienvenido !'
-			redirect_to @user
+			redirect_to login_path
 		else
 			flash[:error] = @user.errors.full_messages.join(',')
 			render 'new'
@@ -23,7 +24,13 @@ class UsersController < ApplicationController
 	def user_params
 		params.require(:user).permit(:username, :first_name, :last_name, 
 																 :birth_date, :email, 
-																 :password, :password_confirmation)	
+																 :password, :password_confirmation, :type)	
 	end
+
+	def set_date_format
+		formatted_date = (params['user']['birth_date']).to_date
+		params['user'].merge(birth_date: formatted_date)
+	end
+	private :set_date_format
 	
 end
