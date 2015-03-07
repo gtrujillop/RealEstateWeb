d286ea4729fb786e48f3cc0e2ee9620c68dbed92
Gaston Trujillo <gastus2@hotmail.com>
2015-03-07 02:40:46 -0500

    Add scenario for user types on sign up.

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -14,21 +14,39 @@ describe "the signin process", :type => :feature do
 
   end
 
-  scenario 'Saves a registered user' do
+  scenario 'Saves a registered user as lease_holder' do
     visit new_user_path
     expect(page).to have_content('Formulario de Registro')
     fill_in 'user_username', with: 'new_user'
     fill_in 'user_first_name', with: 'New'
     fill_in 'user_last_name', with: 'User'
     fill_in 'user_identity', with: '1047401321'
-    fill_in 'user_birth_date', with: '11-09-1988'
+    fill_in 'user_birth_date', with: '09-11-1988'
     fill_in 'user_email', with: 'newuser@mail.com'
     fill_in 'user_password', with: 'blahpassword'
     fill_in 'user_password_confirmation', with: 'blahpassword'
+    select('Vender/Arrendar', from: 'user_type')
+    click_button('Crear mi cuenta')
+    expect(LeaseHolder.all.count).to eq(1)
+    expect(LeaseHolder.first.username).to eq('new_user') 
+  end
 
+  scenario 'Saves a registered user as User' do
+    visit new_user_path
+    expect(page).to have_content('Formulario de Registro')
+    fill_in 'user_username', with: 'new_user'
+    fill_in 'user_first_name', with: 'New'
+    fill_in 'user_last_name', with: 'User'
+    fill_in 'user_identity', with: '1047401321'
+    fill_in 'user_birth_date', with: '09-11-1988'
+    fill_in 'user_email', with: 'newuser@mail.com'
+    fill_in 'user_password', with: 'blahpassword'
+    fill_in 'user_password_confirmation', with: 'blahpassword'
+    select('Comprar/Alquilar', :from => 'user_type')
     click_button('Crear mi cuenta')
     expect(User.all.count).to eq(1)
     expect(User.first.username).to eq('new_user') 
+    expect(User.first.type).to eq('')
   end
 
   scenario 'Login as a registered user' do
