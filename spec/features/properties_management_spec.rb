f8e44b89ed2ee33ffe8ab3c1bed247227cfdd681
Gaston Trujillo <gastus2@hotmail.com>
2015-03-21 00:37:40 -0500

    Green initial spec coverage for properties management integration spec.

---
+++ spec/features/properties_management_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "the properties management", :type => :feature, :js => true do
+  
+  scenario 'Shows the properties menu for the leaseholder' do
+  	page.driver.allow_url("secure.gravatar.com")
+  	user = create(:lease_holder)    
+    visit root_path
+    click_link('iniciar sesión')
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'password'
+    click_button 'Login'
+    expect(page).to have_selector('a.dropdown-toggle')
+    page.find('a.dropdown-toggle').click
+    page.find('li#propiedades.dropdown-submenu').hover
+    click_link('Mis Propiedades')
+  end
+end
