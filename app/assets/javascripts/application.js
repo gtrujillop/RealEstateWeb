816e7723f03a42eb381111099787651ffb8145ab
Gaston Trujillo <gastus2@hotmail.com>
2015-03-28 12:48:01 -0500

    Require jq_chosen instead of scaffold.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require chosen-jquery
-//= require scaffold
+//= require jq_chosen
 //= require turbolinks
 //= require_tree
 //= require bootstrap-sprockets
