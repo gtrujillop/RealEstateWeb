d6cc903206a87eec6325ff8edbe2d10e931685ef
Gaston Trujillo <gastus2@hotmail.com>
2015-04-06 21:47:41 -0500

    Add spec for after_create validations to set is_active and address.

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,12 +2,15 @@ class Property < ActiveRecord::Base
 	Cities.data_path = Rails.root.join('config', 'extras', 'cities')
 
 	attr_accessor :city
+	attr_accessor :location
+
 	validates :area, presence: true, 
 							numericality: true
 	validates :floors_number, presence: true, 
 							numericality: { only_integer: true }
-	validates :address, presence: true
-	validates :is_active, presence: true
+
+  after_save :set_is_active
+  after_save :set_address
 
 	belongs_to :lease_holder
 	belongs_to :neighbor
@@ -16,4 +19,24 @@ class Property < ActiveRecord::Base
 	reverse_geocoded_by :latitude, :longitude
 
 	after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
+	def set_is_active
+		self.update_column(:is_active, true)
+	end
+	private :set_is_active
+
+	def set_address
+		self.update_column(:address, full_address)	
+	end
+	private :set_address
+
+	def full_address
+		location << ", #{city_and_country}"
+	end
+	private :full_address
+
+	def city_and_country
+		city << ", Colombia"
+	end
+	private :city_and_country
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -3,13 +3,8 @@ require 'rails_helper'
 describe Property do
   
   describe "presence validations" do
-
 		it { should validate_presence_of(:area) }
 		it { should validate_presence_of(:floors_number) }
-		it { should validate_presence_of(:address) }
-  	it { should validate_presence_of(:is_active) }
-
-	
 	end
 
 	describe "number validations" do
@@ -23,6 +18,24 @@ describe Property do
 
 	describe "relation with neighbor model" do
 		it { should belong_to(:neighbor) }
+	end
+
+	describe 'after_validations' do
+		it 'builds the property address' do
+			property = Property.new({ area: 65, building_name: 'Castellón de la Palma', 
+				neighbor_id: 1, floor: 307, floors_number: 12, lease_holder_id: 10,
+				is_active: true, city: "Medellín", location: "Carrera 78A # 27-100"})
+			property.save
+			expect(Property.last.address).to eq("Carrera 78A # 27-100, Medellín, Colombia")
+		end
+
+		it 'sets is_active as true' do
+			property = Property.new({ area: 65, building_name: 'Castellón de la Palma', 
+				neighbor_id: 1, floor: 307, floors_number: 12, lease_holder_id: 10,
+				city: "Medellín", location: "Carrera 78A # 27-100"})
+			property.save
+			expect(Property.last.is_active).to be_truthy
+		end
 	end
 	
 
