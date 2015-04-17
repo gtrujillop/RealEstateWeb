5dd6ab26238fd3b52f7fc02bfe27a210a1aea1c9
Gastón Trujillo <gtrujillo@tecnocomfenalco.edu.co>
2015-04-17 18:04:26 -0500

    Fix indenting on consultant & lease_holder specs.

--- spec/models/users/lease_holder_spec.rb
+++ spec/models/users/lease_holder_spec.rb
@@ -1,77 +1,77 @@
 require 'rails_helper'
 
 describe LeaseHolder do
-	describe "password validations" do
-		it "fails because no password" do
-    	expect(LeaseHolder.new({ username: "user_1",birth_date: '11-09-1988', first_name: "Juan", last_name: "Perez", email:'my_email@email.com'}).save).to be false
-  	end
+  describe "password validations" do
+    it "fails because no password" do
+      expect(LeaseHolder.new({ username: "user_1",birth_date: '11-09-1988', first_name: "Juan", last_name: "Perez", email:'my_email@email.com'}).save).to be false
+    end
 
-  	it "fails because password is short" do
-    	expect(LeaseHolder.new({username: "user_1",birth_date: '11-09-1988', first_name: "Juan", last_name: "Perez", password: 'blah', email:'my_email@email.com'}).save).to be false
-  	end
+    it "fails because password is short" do
+      expect(LeaseHolder.new({username: "user_1",birth_date: '11-09-1988', first_name: "Juan", last_name: "Perez", password: 'blah', email:'my_email@email.com'}).save).to be false
+    end
 
-  	it "fails because password confirmation does not match" do
-    	expect(LeaseHolder.new({username: "user_1", first_name: "Juan",birth_date: '11-09-1988', last_name: 
-    						"Perez", password: 'password_1', password_confirmation: 'password_2', email:'my_email@email.com'}).save).to be false
-  	end
+    it "fails because password confirmation does not match" do
+      expect(LeaseHolder.new({username: "user_1", first_name: "Juan",birth_date: '11-09-1988', last_name:
+                              "Perez", password: 'password_1', password_confirmation: 'password_2', email:'my_email@email.com'}).save).to be false
+    end
 
-  	it "saves because password is valid" do
-    	expect(LeaseHolder.new({username: "user_1",birth_date: '11-09-1988', first_name: "Juan", last_name: 
-    						"Perez", password: 'password_1', password_confirmation: 'password_1', email:'my_email@email.com'}).save).to be true
-  	end
-	end
+    it "saves because password is valid" do
+      expect(LeaseHolder.new({username: "user_1",birth_date: '11-09-1988', first_name: "Juan", last_name:
+                              "Perez", password: 'password_1', password_confirmation: 'password_1', email:'my_email@email.com'}).save).to be true
+    end
+  end
 
-	describe "presence validations" do
-		it { should validate_presence_of(:email) }
-		it { should validate_presence_of(:password) }
-		it { should validate_presence_of(:password_confirmation) }
-	end
+  describe "presence validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
+  end
 
-	describe "format validations" do
+  describe "format validations" do
 
-		it do
-			should allow_value('my_email@email.com').
-				for(:email).
-				on(:create)	
-		end
+    it do
+      should allow_value('my_email@email.com').
+        for(:email).
+          on(:create)
+      end
 
-		it { should validate_length_of(:password).is_at_least(3) }
+      it { should validate_length_of(:password).is_at_least(3) }
 
 
-	end
+    end
 
-	describe "date validations" do
-		it 'does not allow save users with invalid dates' do
-    	expect(LeaseHolder.new({ username: "user_1", first_name: "Juan", 
-    										last_name: "Perez", birth_date: 'blah', 
-    										email:'my_email@email.com', 
-    										password: 'password_1', 
-    										password_confirmation: 'password_1'}).save).to be false
-		end
+    describe "date validations" do
+      it 'does not allow save users with invalid dates' do
+        expect(LeaseHolder.new({ username: "user_1", first_name: "Juan",
+                                 last_name: "Perez", birth_date: 'blah',
+                                 email:'my_email@email.com',
+                                 password: 'password_1',
+                                 password_confirmation: 'password_1'}).save).to be false
+      end
 
-		it 'allows save users with valid dates' do
-    	expect(LeaseHolder.new({ username: "user_1", first_name: "Juan", 
-    										last_name: "Perez", birth_date: '11-09-1988', 
-    										email:'my_email@email.com', 
-    										password: 'password_1', 
-    										password_confirmation: 'password_1'}).save).to be true
-		end
-	end
+      it 'allows save users with valid dates' do
+        expect(LeaseHolder.new({ username: "user_1", first_name: "Juan",
+                                 last_name: "Perez", birth_date: '11-09-1988',
+                                 email:'my_email@email.com',
+                                 password: 'password_1',
+                                 password_confirmation: 'password_1'}).save).to be true
+      end
+    end
 
-	describe "relation with property model" do
-		it { should have_many(:properties) }
-	end
+    describe "relation with property model" do
+      it { should have_many(:properties) }
+    end
 
-	describe 'after validations' do
-		it 'sets active value before saving the user' do
-			user = Consultant.new({ username: "user_1", first_name: "Juan", 
-    										last_name: "Perez", birth_date: '11-09-1988', 
-    										email:'my_email@email.com', 
-    										password: 'password_1', 
-    										password_confirmation: 'password_1'})
-			user.save
-			expect(Consultant.last.active).to be_truthy
-		end
-	end
+    describe 'after validations' do
+      it 'sets active value before saving the user' do
+        user = Consultant.new({ username: "user_1", first_name: "Juan",
+                                last_name: "Perez", birth_date: '11-09-1988',
+                                email:'my_email@email.com',
+                                password: 'password_1',
+                                password_confirmation: 'password_1'})
+        user.save
+        expect(Consultant.last.active).to be_truthy
+      end
+    end
 
-end
+  end
