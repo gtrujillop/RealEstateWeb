55307b9e6e37af448e56ba0806760d8ccb20ac50
Gastón Trujillo <gtrujillo@tecnocomfenalco.edu.co>
2015-04-24 22:49:35 -0500

    Partial fix on integration spec for Property management.

--- spec/features/properties_management_spec.rb
+++ spec/features/properties_management_spec.rb
@@ -1,17 +1,13 @@
 require 'rails_helper'
 
 describe "the properties management", :type => :feature, :js => true do
+  let(:user) { create(:lease_holder) }
+
+  before do
+    login_me(user)
+  end
 
   scenario 'Shows the index properties for the leaseholder' do
-    #TODO create an alternative scenario and
-    #expectation to check that property attrs are rendered.
-    page.driver.allow_url("secure.gravatar.com")
-    user = create(:lease_holder)
-    visit root_path
-    click_link('iniciar sesión')
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
-    click_button 'Login'
     expect(page).to have_selector('a.dropdown-toggle')
     page.find('a.dropdown-toggle').click
     page.find('li#propiedades.dropdown-submenu').hover
@@ -20,13 +16,6 @@ describe "the properties management", :type => :feature, :js => true do
   end
 
   scenario 'Shows the properties menu for the leaseholder' do
-    page.driver.allow_url("secure.gravatar.com")
-    user = create(:lease_holder)
-    visit root_path
-    click_link('iniciar sesión')
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
-    click_button 'Login'
     expect(page).to have_selector('a.dropdown-toggle')
     page.find('a.dropdown-toggle').click
     page.find('li#propiedades.dropdown-submenu').hover
@@ -35,25 +24,15 @@ describe "the properties management", :type => :feature, :js => true do
   end
 
   scenario 'shows the property registration form' do
-    page.driver.allow_url("secure.gravatar.com")
-    user = create(:lease_holder)
-    visit root_path
-    click_link('iniciar sesión')
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
-    click_button 'Login'
-    page.find('a.dropdown-toggle').click
-    page.find('li#propiedades.dropdown-submenu').hover
-    click_link('Registrar Propiedad')
-    fill_in 'user_username', with: 'new_user'
-    fill_in 'user_first_name', with: 'New'
-    fill_in 'user_last_name', with: 'User'
-    fill_in 'user_identity', with: '1047401321'
-    fill_in 'user_birth_date', with: '09/11/1988'
-    fill_in 'user_email', with: 'newuser@mail.com'
-    fill_in 'user_password', with: 'blahpassword'
-    fill_in 'user_password_confirmation', with: 'blahpassword'
-    select('Vender/Arrendar', from: 'user_type')
-    click_button('Crear mi cuenta')
+    visit new_user_property_path(user.id)
+    page.select 'Cartagena', :from => 'property_city'
+    fill_in 'property_neighbor', with: 'Torices'
+    fill_in 'property_location', with: 'Calle 42 # 14B-2 a 14B-100'
+    fill_in 'property_area', with: '100'
+    fill_in 'property_floors_number', with: 1
+    fill_in 'property_floor', with: 1
+    fill_in 'property_building_name', with: ''
+    click_button('Registrar')
+    expect(Property.last).to_not be_nil
   end
 end
