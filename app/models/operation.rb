afdd992c735e4b5cdcb72d05a058d636d60c8e98
Gastón Trujillo <gtrujillo@tecnocomfenalco.edu.co>
2015-05-21 17:58:15 -0500

    Add Operation and green spec coverage.

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,7 +1,7 @@
 class Operation < ActiveRecord::Base
 
-	validates :start_date, presence: true
-  validates :end_date, presence: true
+	validates :start_date, presence: true, date: true
+  validates :end_date, presence: true, date: true
   validates :payment, presence: true, numericality: true
 
   after_save :set_is_active
@@ -13,5 +13,15 @@ class Operation < ActiveRecord::Base
 		self.update_column(:is_active, true)
 	end
 	private :set_is_active
+
+  def set_duration
+    self.update_column(:duration, duration_from_dates)
+  end
+  private :set_duration
+
+  def duration_from_dates
+    DateProcessor.new(start_date).months_between(end_date).to_s + " months"
+  end
+  private :duration_from_dates
 
 end
--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -9,6 +9,10 @@ describe Operation do
       it { should validate_presence_of(:payment) }
     end
 
+    describe "date validations" do
+
+    end
+
     describe "relation with property model" do
       it { should belong_to(:property) }
     end
@@ -17,7 +21,14 @@ describe Operation do
 
   describe 'callbacks' do
     it 'sets the duration value based on start - end date ranges' do
-
+      operation = Operation.new({start_date: Date.today, end_date: 12.months.from_now.to_date, payment: 1000})
+      operation.save
+      expect(operation.duration).to eq('12 months')
+    end
+    it 'sets the is_active value as active after save' do
+      operation = Operation.new({start_date: Date.today, end_date: 12.months.from_now.to_date, payment: 1000})
+      operation.save
+      expect(operation.is_active).to eq(true)
     end
   end
 end
