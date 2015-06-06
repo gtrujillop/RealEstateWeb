cd59d61c76c41c7057d8853e957f1517bbfa8716
Gastón Trujillo <gtrujillo@tecnocomfenalco.edu.co>
2015-06-05 23:12:14 -0500

    Remove Neighbor model.

--- app/models/neighbor.rb
+++
@@ -1,8 +0,0 @@
-class Neighbor < ActiveRecord::Base
-
-	validates :name, presence: true, length: { maximum: 140 }
-	validates :stratus, presence: true, numericality: { only_integer: true }
-
-	belongs_to :zone
-
-end
