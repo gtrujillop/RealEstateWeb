7dde8013648be76aee4ec291638d0437084634d0
Gastón Trujillo <gtrujillo@tecnocomfenalco.edu.co>
2015-06-10 22:18:08 -0500

    Add presence validation for image field.

--- app/models/property.rb
+++ app/models/property.rb
@@ -14,7 +14,7 @@ class Property < ActiveRecord::Base
 
   validates :floors_number, presence: true,
               numericality: { only_integer: true }
-
+  #TODO set numericality validation without break the spec.
   # validates :value_for_sell, numericality: true
   # validates :value_for_rental, numericality: true
 
--- app/models/property_element.rb
+++ app/models/property_element.rb
@@ -4,6 +4,7 @@ class PropertyElement < ActiveRecord::Base
 
   validates :name, presence: true
   validates :description, presence: true
+  validates :image, presence: true
 
   has_attached_file :image, :styles => { :medium => "400x400>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
