ea30df0db839961c2e3e0adc2da3f109113087fa
Gastón Trujillo <gtrujillo@tecnocomfenalco.edu.co>
2015-06-12 16:34:46 -0500

    Show icons for each element_type found.

--- app/presenters/property_element_presenter.rb
+++ app/presenters/property_element_presenter.rb
@@ -18,8 +18,25 @@ class PropertyElementPresenter
 
   def types_for_property
     @types_for_property ||= ElementType.all.each_with_object({}) do |type, hsh|
-      hsh[type.name] = PropertyElement.where(property_id: property.id, element_type_id: type.id).count
+      hsh[type.name] = count_by_type(type.id)
     end
   end
+
+  def glyphicon_for_type(type_name)
+    case type_name
+    when 'Baños' then 'glyphicon glyphicon-ok-circle'
+    when 'Habitaciones' then 'glyphicon glyphicon-ok-circle'
+    when 'Cocinas' then 'glyphicon glyphicon-cutlery'
+    when 'Garages' then 'glyphicon glyphicon-car-rental'
+    when 'Gimnasios' then 'glyphicon glyphicon-dumbbell'
+    when 'Piscina' then 'glyphicon glyphicon-pool'
+    else 'glyphicon glyphicon-home'
+    end
+  end
+
+  def count_by_type(type_id)
+    PropertyElement.where(property_id: property.id, element_type_id: type_id).count
+  end
+  private :count_by_type
 
 end
