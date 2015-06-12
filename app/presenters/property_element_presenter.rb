0750bead0b06c95b56b39d43789ae57503ff63b5
Gastón Trujillo <gtrujillo@tecnocomfenalco.edu.co>
2015-06-11 22:32:40 -0500

    Initial method to show elements by type count on Property show view.

--- app/presenters/property_element_presenter.rb
+++ app/presenters/property_element_presenter.rb
@@ -16,4 +16,20 @@ class PropertyElementPresenter
     end
   end
 
+  def types_for_property
+    @types_for_property ||= property.property_elements.all.each_with_object({}) do |memo, hsh|
+      hsh[type_name_for_element(memo)] = element_count_for_type(memo.id)
+    end
+  end
+
+  def element_count_for_type(type_id)
+    PropertyElement.where(element_type: type_id).count
+  end
+  private :element_count_for_type
+
+  def type_name_for_element(element)
+    ElementType.find(element.element_type_id).name
+  end
+  private :type_name_for_element
+
 end
