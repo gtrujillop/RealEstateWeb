4e9d65f1255134b276ee1cc0e3b3cd13732dcce1
Gastón Trujillo <gtrujillo@tecnocomfenalco.edu.co>
2015-06-19 23:46:37 -0500

    Add new db schema for properties table.

--- db/migrate/20150610033156_add_value_to_properties.rb
+++
@@ -1,6 +0,0 @@
-class AddValueToProperties < ActiveRecord::Migration
-  def change
-    add_column :properties, :value_for_sell, :float
-    add_column :properties, :value_for_rental, :float
-  end
-end
