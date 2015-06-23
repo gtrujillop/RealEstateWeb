bcba57f289ec138a971bdba71bf6c5d5143e2f03
Gastón Trujillo <gtrujillo@tecnocomfenalco.edu.co>
2015-06-22 22:20:29 -0500

    Add gmaps to application js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require chosen-jquery
