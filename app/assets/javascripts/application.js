57346ed81af3deeb0177cb1324dbed7e67f06cd8
Gastón Trujillo <gtrujillo@tecnocomfenalco.edu.co>
2015-06-22 22:26:02 -0500

    remove require_tree from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require_tree
+//
 //= require jquery
 //= require jquery_ujs
 //= require chosen-jquery
