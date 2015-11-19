1560f2aad9326208e65310d626071114e2c66100
gtrujillop <gaston.trujillo.java@gmail.com>
2015-11-18 22:54:44 -0500

    js: remove or add hidden class for filtering opt divs.

--- app/assets/javascripts/property_filtering_control.js
+++ app/assets/javascripts/property_filtering_control.js
@@ -1,19 +1,13 @@
-$(document).ready(function(){
-  console.log('filters are hidden');
-  $('.prop_filter').hide();
-  $('.prop_filter_by_map').hide();
-});
-
 $(document).on('click', '.prop_filter_by_map_button', function(){
-    $('.prop_filter_by_map').show();
-    $('.prop_filter').hide();
+    $('#prop_filter_by_map').removeClass('hidden');
+    $('#prop_filter').addClass('hidden');
     $('.prop_filter_by_map_button').hide();
     $('.prop_filter_button').show();
 });
 
 $(document).on('click', '.prop_filter_button', function(){
-    $('.prop_filter').show();
-    $('.prop_filter_by_map').hide();
+    $('#prop_filter').removeClass('hidden');
+    $('#prop_filter_by_map').addClass('hidden');
     $('.prop_filter_button').hide();
     $('.prop_filter_by_map_button').show();
 });
